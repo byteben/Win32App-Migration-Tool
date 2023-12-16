86999b0e42da1d7addd536dddcb160a127108ffb
Ben Whitmore <44303264+byteben@users.noreply.github.com>
2023-12-16 14:39:21 +0000

    Now we roll exported csv files up to an archive

--- Private/Export-CsvDetails.ps1
+++ Private/Export-CsvDetails.ps1
@@ -1,6 +1,7 @@
 <#
 .Synopsis
 Created on:   05/11/23
+Updated on:   16/12/23
 Created by:   Ben Whitmore
 Filename:     Export-Csv.ps1
 
@@ -36,25 +37,39 @@ function Export-CsvDetails {
 
     begin {
 
+        # Specify CSV archive folder name
+        $archiveFolder = 'Old'
+
         # Log the export
         Write-Log -Message ("Exporting '{0}' information to '{1}\{0}.csv'" -f $Name, $Path) -LogId $LogId
         Write-Host ("Exporting '{0}' information to '{1}\{0}.csv'" -f $Name, $Path) -ForegroundColor Cyan
 
         # Build path string
-        $Path = Join-Path -Path $Path -ChildPath ("{0}.csv" -f $Name)
+        $fullPath = Join-Path -Path $Path -ChildPath ("{0}.csv" -f $Name)
+        $archivePath = Join-Path -Path $Path -ChildPath $archiveFolder
 
         # Rollover the csv if it already exists from a previous run
-        if (Test-Path -Path $Path) {
-            Write-Log -Message ("'{0}\{1}.csv' already exists, rolling over csv" -f $Path, $Name) -LogId $LogId -Severity 2
-            Write-Host ("'{0}' already exists, rolling over csv" -f $Path) -ForegroundColor Yellow
+        if (Test-Path -Path $fullPath) {
+            Write-Log -Message ("'{0}' already exists, rolling over csv" -f $fullPath) -LogId $LogId -Severity 2
+            Write-Host ("'{0}' already exists, rolling over csv" -f $fullPath) -ForegroundColor Yellow
             $date = Get-Date -Format 'yyyyMMddHHmmss'
-            $pathRename = $Path -replace '.csv', ''
+            $pathRename = $fullPath -replace '.csv', ''
 
             # Attempt to rename the file, if it fails continue but the file will be overwritten
             try {
-                Rename-Item -Path $Path -NewName ("{0}_{1}.csv" -f $pathRename, $date)
-                Write-Log -Message ("Previous Csv rolled over to '{0}_{1}.csv'" -f $pathRename, $date) -LogId $LogId
-                Write-Host ("Previous Csv rolled over to '{0}_{1}.csv'" -f $pathRename, $date) -ForegroundColor Green
+                $newPath = ("{0}_{1}.csv" -f $pathRename, $date)
+                Rename-Item -Path $fullPath -NewName $newPath
+                
+                # Create the archive folder if it does not exist
+                if (-not (Test-Path -Path $archivePath)){
+                    New-FolderToCreate -Root $Path -FolderNames $archiveFolder
+                }
+
+                # Move the old CSV into the archive folder
+                Move-Item -Path $newPath -Destination $archivePath -Force
+
+                Write-Log -Message ("Previous Csv rolled over to '{0}\{1}_{2}.csv'" -f $archivePath, $pathRename, $date) -LogId $LogId
+                Write-Host ("Previous Csv rolled over to '{0}\{1}_{2}.csv'" -f $archivePath, $pathRename, $date) -ForegroundColor Green
             }
             catch {
                 Write-Log -Message ("Failed to rollover '{0}'. Csv will be overwritten." -f $Path) -LogId $LogId -Severity 3
@@ -70,20 +85,20 @@ function Export-CsvDetails {
             try {
 
                 # Check if the file already exists to handle the header correctly
-                if (-not (Test-Path -Path $Path) ) {
+                if (-not (Test-Path -Path $fullPath) ) {
 
                     # Include headers if the file doesn't exist
-                    $object | ConvertTo-Csv -NoTypeInformation | Out-File -FilePath $Path
+                    $object | ConvertTo-Csv -NoTypeInformation | Out-File -FilePath $fullPath
                 }
                 else {
 
                     # If the file exists, skip the header line
-                    $object | ConvertTo-Csv -NoTypeInformation | Select-Object -Skip 1 | Out-File -FilePath $Path -Append
+                    $object | ConvertTo-Csv -NoTypeInformation | Select-Object -Skip 1 | Out-File -FilePath $fullPath -Append
                 }
             }
             catch {
-                Write-Log -Message ("Failed to export '{0}' information to '{1}'" -f $Type, $Path) -LogId $LogId -Severity 3
-                Write-Host ("Failed to export '{0}' information to '{1}'" -f $Type, $Path) -ForegroundColor Red
+                Write-Log -Message ("Failed to export '{0}' information to '{1}'" -f $Type, $fullPath) -LogId $LogId -Severity 3
+                Write-Host ("Failed to export '{0}' information to '{1}'" -f $Type, $fullPath) -ForegroundColor Red
                 Write-Log -Message ("'{0}'" -f $_.Exception.Message) -LogId $LogId -Severity 3
                 throw
             }
