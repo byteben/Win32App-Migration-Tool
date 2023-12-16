007f1892326bfb297fcc800d6b583283772cd529
Ben Whitmore <44303264+byteben@users.noreply.github.com>
2023-12-16 10:07:36 +0000

    Formating change and now will throw on error ```

--- Win32AppMigrationTool.psm1
+++ Win32AppMigrationTool.psm1
@@ -1,21 +1,22 @@
 <#
 .Synopsis
-Created on:   12/11/2023
+Created on:   16/12/2023
 Created by:   Ben Whitmore
 Filename:     Win32AppMigrationTool.psm1
 
 .Description
 Win32App Packaging Tool Function Import
 #>
+
 $PublicFunctions = @( Get-ChildItem -Path $PSScriptRoot\Public\*.ps1 -Recurse -ErrorAction SilentlyContinue )
 $PrivateFunctions = @( Get-ChildItem -Path $PSScriptRoot\Private\*.ps1 -Recurse -ErrorAction SilentlyContinue )
 
 foreach ($GetFunction in @($PublicFunctions + $PrivateFunctions)) {
-    Try {
+    try {
         . $GetFunction.FullName
     }
-    Catch {
-        Write-Host "Failed to import function $($GetFunction.FullName)" -ForegroundColor Red
-        Write-Host "$_" -ForegroundColor Yellow
+    catch {
+        Write-Error -Message ("Failed to import function '{0}'" -f $GetFunction.FullName)
+        throw
     }
 }
