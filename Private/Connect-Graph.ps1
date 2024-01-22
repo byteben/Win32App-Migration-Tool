3801659370edf309968e90eaff6341b6cff53cb1
Ben Whitmore <44303264+byteben@users.noreply.github.com>
2024-01-22 17:29:13 +0000

    updating connect-graph in dev

--- Private/Connect-Graph.ps1
+++ Private/Connect-Graph.ps1
@@ -12,12 +12,6 @@ Function to connect to Microsoft Graph
 The component (script name) passed as LogID to the 'Write-Log' function. 
 This parameter is built from the line number of the call from the function up the pipeline
 
-.PARAMETER ClientID
-ClientID. If not specified, the default value is used for Microsoft Intune PoSh
-
-.PARAMETER RedirectUri
-Redirect URI. If not specified, the default value for OAuth2 is used
-
 .PARAMETER ModuleName
 Module Name to connect to Graph. If not specified, the default value is used for Microsoft.Graph
 
@@ -27,24 +21,28 @@ Package Provider. If not specified, the default value NuGet is used
 .PARAMETER ModuleScope
 Module Scope. If not specified, the default value is used for CurrentUser
 
+.PARAMETER Scopes
+The scopes to request from the Microsoft Graph API. If not specified, the default value is used for Application.ReadWrite.All
+
+.PARAMETER UseDeviceAuthentication
+This parameter will be used to determine if the device authentication flow should be used. If not specified, the default value is used for $false
+
 #>
 function Connect-Graph {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = "The component (script name) passed as LogID to the 'Write-Log' function")]
+        [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = 'The component (script name) passed as LogID to the "Write-Log" function')]
         [string]$LogId = $($MyInvocation.MyCommand).Name,
-        [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = "Module Name to connect to Graph. If not specified, the default value is used for Microsoft.Graph")]
+        [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = 'Module Name to connect to Graph. If not specified, the default value is used for Microsoft.Graph')]
         [string]$ModuleName = 'Microsoft.Graph',
-        [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = "If not specified, the default value NuGet is used for PackageProivder")]
+        [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = 'If not specified, the default value NuGet is used for PackageProivder')]
         [string]$PackageProivder = 'NuGet',
-        [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = "If not specified, the default value is used for CurrentUser")]
+        [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = 'If not specified, the default value is used for CurrentUser')]
         [string]$ModuleScope = 'CurrentUser',
-        [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = "If not specified, the default value is used for Microsoft Intune PoSh")]
-        [string]$ClientId = 'd1ddf0e4-d672-4dae-b554-9d5bdfd93547',
-        [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = "If not specified, the default value for OAuth2 is used")]
-        [string]$RedirectUri = 'urn:ietf:wg:oauth:2.0:oob',
-        [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = "The scopes to request from the Microsoft Graph API. If not specified, the default value is used for Application.ReadWrite.All")]
-        [string]$Scopes = 'Application.ReadWrite.All'
+        [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = 'The scopes to request from the Microsoft Graph API. If not specified, the default value is used for Application.ReadWrite.All')]
+        [string]$Scopes = 'DeviceManagementApps.ReadWrite.All',
+        [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = 'This parameter will be used to determine if the device authentication flow should be used. If not specified, the default value is used for $false')]
+        [switch]$UseDeviceAuthentication = $false
     )
 
     begin {
@@ -68,7 +66,7 @@ function Connect-Graph {
         }
 
         # Check if the module is installed
-        if (-not (Get-Module -ListAvailable -Name Microsoft.Graph)) {
+        if (-not (Get-Module -ListAvailable -Name $ModuleName)) {
 
             # Install the module if it's not already installed
             Write-Log -Message ("Module not found. Will install module '{0}' in the scope of '{1}'" -f $ModuleName, $ModuleScope) -LogId $LogId
@@ -89,24 +87,41 @@ function Connect-Graph {
             Write-Log -Message ("Module '{0}' is already installed" -f $ModuleName) -LogId $LogId
             Write-Host ("Module '{0}' is already installed" -f $ModuleName) -ForegroundColor Cyan
 
-            try {
-                Write-Log -Message ("Import-Module {0}" -f $ModuleName) -LogId $LogId
-                Write-Host ("Importing Module: '{0}'" -f $ModuleName) -ForegroundColor Cyan
-                Import-Module Microsoft.Graph
-            }
-            catch {
-                Write-Log -Message ("Warning: Could not import the module '{0}'" -f $ModuleName) -LogId $LogId -Severity 3
-                Write-Warning ("Warning: Could not import the module '{0}'" -f $ModuleName)
-                Write-Log -Message ("'{0}'" -f $_.Exception.Message) -LogId $LogId -Severity 3
-                Get-ScriptEnd -Message $_.Exception.Message -LogId $LogId 
+            if (-not (Get-Module -Name $ModuleName)) {
+
+                try {
+                    Write-Log -Message ("Import-Module {0}" -f $ModuleName) -LogId $LogId
+                    Write-Host ("Importing Module: '{0}'" -f $ModuleName) -ForegroundColor Cyan
+                    Import-Module Microsoft.Graph
+                }
+                catch {
+                    Write-Log -Message ("Warning: Could not import the module '{0}'" -f $ModuleName) -LogId $LogId -Severity 3
+                    Write-Warning ("Warning: Could not import the module '{0}'" -f $ModuleName)
+                    Write-Log -Message ("'{0}'" -f $_.Exception.Message) -LogId $LogId -Severity 3
+                    Get-ScriptEnd -Message $_.Exception.Message -LogId $LogId 
+                }
+                else {
+                    Write-Log -Message ("Module '{0}' is imported into PowerShell session" -f $ModuleName) -LogId $LogId
+                    Write-Host ("Module '{0}' is imported into PowerShell session" -f $ModuleName) -ForegroundColor Cyan
+                }
             }
         }
     }
 
     process {
+
+        # Build connection splat
+        $ConnectGraphSplat = @{
+            Scopes    = $Scopes
+            NoWelcome = $true
+        }
+
+        if ($PSBoundParameters["UseDeviceAuthentication"]) {
+            $connectGraphSplat.Add('UseDeviceAuthentication', $true)
+        }
         
         # Connect to Microsoft Graph
-        Connect-MgGraph -Scopes $scope -NoWelcome
+        Connect-MgGraph $ConnectGraphSplat
 
         # Check if the connection is successful
         if (Get-MgContext) {
