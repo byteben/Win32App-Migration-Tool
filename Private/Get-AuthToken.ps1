704d1ea07f2199a361f53d5ab253ef6fe6dffeca
Ben Whitmore <44303264+byteben@users.noreply.github.com>
2024-04-02 18:41:21 +0100

    Fixed some syntax issues and removed comment #flag ```

--- Private/Get-AuthToken.ps1
+++ Private/Get-AuthToken.ps1
@@ -37,6 +37,9 @@ RedirectUri to use for Client Id. If not specified, a default value is used base
 .PARAMETER Scopes
 The scopes to request from the Microsoft Graph API. If not specified, the default value is used for Application.ReadWrite.All
 
+.PARAMETER RefreshToken
+If specified, the token will be renewed
+
 .PARAMETER UseDeviceAuthentication
 This parameter will be used to determine if the device authentication flow should be used. If not specified, the default value is used for $false
 #>
@@ -59,29 +62,31 @@ function Get-AuthToken {
         [string]$ModuleScope = 'CurrentUser',
         [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = 'RedirectUri to use for Client Id')]
         [string]$RedirectUri,
-        [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = 'The scopes to request from the Microsoft Graph API. If not specified, the default value is used for Application.ReadWrite.All')]
+        [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = 'The scopes to request from the Microsoft Graph API')]
         [object]$Scopes = @('https://graph.microsoft.com/.default'),
+        [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = 'Specify if the token should be renewed. If not specified, the default value is used for $false')]
+        [Switch]$RefreshToken,
         [Parameter(Mandatory = $false, ValuefromPipeline = $false, HelpMessage = 'This parameter will be used to determine if the device authentication flow should be used. If not specified, the default value is used for $false')]
         [switch]$UseDeviceAuthentication = $false
     )
 
     begin {
-        #Write-Log -Message 'Function: Get-AuthToken was called' -LogId $LogId
+        Write-Log -Message 'Function: Get-AuthToken was called' -LogId $LogId
 
         if (-not (Get-PackageProvider -ListAvailable -Name $PackageProvider)) {
 
             # Install the PackageProvider if it's not already installed
-            #Write-Log -Message ("PackageProvider not found. Will install PackageProvider '{0}'" -f $PackageProvider) -LogId $LogId
+            Write-Log -Message ("PackageProvider not found. Will install PackageProvider '{0}'" -f $PackageProvider) -LogId $LogId
             Write-Host ("Installing PackageProvider '{0}'" -f $PackageProvider) -ForegroundColor Cyan
     
             try {
                 Install-PackageProvider -Name $PackageProvider -ForceBootstrap -Confirm:$false -Verbose
             }
             catch {
-                #Write-Log -Message ("Warning: Could not install the PackageProvider '{0}'" -f $PackageProvider) -LogId $LogId -Severity 3
+                Write-Log -Message ("Warning: Could not install the PackageProvider '{0}'" -f $PackageProvider) -LogId $LogId -Severity 3
                 Write-Warning ("Warning: Could not install the PackageProvider '{0}'" -f $PackageProvider)
-                #Write-Log -Message ("'{0}'" -f $_.Exception.Message) -LogId $LogId -Severity 3
-                #Get-ScriptEnd -ErrorMessage $_.Exception.Message -LogId $LogId 
+                Write-Log -Message ("'{0}'" -f $_.Exception.Message) -LogId $LogId -Severity 3
+                Get-ScriptEnd -ErrorMessage $_.Exception.Message -LogId $LogId 
             }
         }
 
@@ -90,40 +95,40 @@ function Get-AuthToken {
             if (-not (Get-Module -ListAvailable -Name $ModuleName)) {
 
                 # Install the module if it's not already installed
-                #Write-Log -Message ("Module not found. Will install module '{0}' in the scope of '{1}'" -f $ModuleName, $ModuleScope) -LogId $LogId
+                Write-Log -Message ("Module not found. Will install module '{0}' in the scope of '{1}'" -f $ModuleName, $ModuleScope) -LogId $LogId
                 Write-Host ("Installing module '{0}' in the scope of '{1}'" -f $ModuleName, $ModuleScope) -ForegroundColor Cyan
     
                 try {
                     Install-Module -Name $ModuleName -Scope $ModuleScope -AllowClobber -Force -Confirm:$false
                 }
                 catch {
-                    #Write-Log -Message ("Warning: Could not install the module '{0}'" -f $ModuleName) -LogId $LogId -Severity 3
+                    Write-Log -Message ("Warning: Could not install the module '{0}'" -f $ModuleName) -LogId $LogId -Severity 3
                     Write-Warning ("Warning: Could not install the module '{0}'" -f $ModuleName)
-                    #Write-Log -Message ("'{0}'" -f $_.Exception.Message) -LogId $LogId -Severity 3
-                    #Get-ScriptEnd -ErrorMessage $_.Exception.Message -LogId $LogId
+                    Write-Log -Message ("'{0}'" -f $_.Exception.Message) -LogId $LogId -Severity 3
+                    Get-ScriptEnd -ErrorMessage $_.Exception.Message -LogId $LogId
                 }
             }
             else {
                 # Module is already installed, import it
-                #Write-Log -Message ("Module '{0}' is already installed" -f $ModuleName) -LogId $LogId
+                Write-Log -Message ("Module '{0}' is already installed" -f $ModuleName) -LogId $LogId
                 Write-Host ("Module '{0}' is already installed" -f $ModuleName) -ForegroundColor Cyan
 
                 if (-not (Get-Module -Name $ModuleName)) {
 
                     try {
-                        #Write-Log -Message ("Import-Module {0}" -f $ModuleName) -LogId $LogId
+                        Write-Log -Message ("Import-Module {0}" -f $ModuleName) -LogId $LogId
                         Write-Host ("Importing Module: '{0}'" -f $ModuleName) -ForegroundColor Cyan
                         Import-Module $ModuleName
                     }
                     catch {
-                        #Write-Log -Message ("Warning: Could not import the module '{0}'" -f $ModuleName) -LogId $LogId -Severity 3
+                        Write-Log -Message ("Warning: Could not import the module '{0}'" -f $ModuleName) -LogId $LogId -Severity 3
                         Write-Warning ("Warning: Could not import the module '{0}'" -f $ModuleName)
-                        #Write-Log -Message ("'{0}'" -f $_.Exception.Message) -LogId $LogId -Severity 3
-                        #Get-ScriptEnd -ErrorMessage $_.Exception.Message -LogId $LogId 
+                        Write-Log -Message ("'{0}'" -f $_.Exception.Message) -LogId $LogId -Severity 3
+                        Get-ScriptEnd -ErrorMessage $_.Exception.Message -LogId $LogId 
                     }
                 }
                 else {
-                    #Write-Log -Message ("Module '{0}' is imported into PowerShell session" -f $ModuleName) -LogId $LogId
+                    Write-Log -Message ("Module '{0}' is imported into PowerShell session" -f $ModuleName) -LogId $LogId
                     Write-Host ("Module '{0}' is imported into PowerShell session" -f $ModuleName) -ForegroundColor Cyan
                 }
             }
@@ -133,7 +138,7 @@ function Get-AuthToken {
     process {
 
         # Check if the redirect URI is specified, if not set it depending on the PowerShell version
-        #Write-Log -Message ("PowerShell version is '{0}'" -f $PSVersionTable.PSVersion) -LogId $LogId
+        Write-Log -Message ("PowerShell version is '{0}'" -f $PSVersionTable.PSVersion) -LogId $LogId
         Write-Host ("PowerShell version is '{0}'" -f $PSVersionTable.PSVersion) -ForegroundColor Cyan
 
         if ($PSVersionTable.PSVersion.Major -lt 7) {
@@ -143,7 +148,7 @@ function Get-AuthToken {
             $RedirectUri = "http://localhost"
         }
 
-        #Write-Log -Message ("Setting redirect URI to '{0}'" -f $RedirectUri) -LogId $LogId
+        Write-Log -Message ("Setting redirect URI to '{0}'" -f $RedirectUri) -LogId $LogId
 
         # Build connection splat
         $tokenSplat = [ordered]@{
@@ -161,9 +166,14 @@ function Get-AuthToken {
             $tokenSplat.Add('DeviceCode', $true)
         }
 
+        # Check if we need to renew the token
+        if ($PSBoundParameters["RefreshToken"]) {
+            $tokenSplat.Add("ForceRefresh", $true)
+        }
+
         # Attempt to obtain an access token
         try {
-            #Write-Log -Message ("Attempting to connect. Token Splat: '{0}'" -f $tokenSplat ) -LogId $LogId
+            Write-Log -Message ("Attempting to connect. Token Splat: '{0}'" -f $tokenSplat ) -LogId $LogId
             Write-Host "Attempting to connect..." -ForegroundColor Cyan
             foreach ($key in $tokenSplat.Keys) {
                 Write-Host ("{0}: {1}" -f $key, $($tokenSplat[$key])) -ForegroundColor Cyan
@@ -173,16 +183,16 @@ function Get-AuthToken {
         }
         catch {
             Write-Warning -Message ("'{0}'" -f $_.Exception.Message)
-            #Write-Log -Message ("'{0}'" -f $_.Exception.Message) -LogId $LogId -Severity 3
-            #Get-ScriptEnd -ErrorMessage 'Failed to connect to get a token' -LogId $LogId
+            Write-Log -Message ("'{0}'" -f $_.Exception.Message) -LogId $LogId -Severity 3
+            Get-ScriptEnd -ErrorMessage 'Failed to get a token' -LogId $LogId
         }
 
         # Check if the connection is successful
         if ($token) {
-            #Write-Log -Message 'Successfully connected' -LogId $LogId
-            #Write-Log -Message ("Token expires on: {0}" -f $token.ExpiresOn) -LogId $LogId
+            Write-Log -Message 'Successfully connected' -LogId $LogId
+            Write-Log -Message ("Token expires on: {0}" -f $token.ExpiresOn.UtcDateTime) -LogId $LogId
             Write-Host 'Successfully connected' -ForegroundColor Green
-            Write-Host ("Token expires on: {0}" -f $token.ExpiresOn) -ForegroundColor Green
+            Write-Host ("Token expires on (UTC): {0}" -f $token.ExpiresOn.UtcDateTime) -ForegroundColor Green
 
             # Create the authentication header
             try {
@@ -191,18 +201,18 @@ function Get-AuthToken {
                     "Authorization" = $token.CreateAuthorizationHeader()
                     "ExpiresOn"     = $token.ExpiresOn.UTCDateTime
                 }
-                #Write-Log -Message 'Successfully created authentication header' -LogId $LogId
-                #Write-Log -Message ("Authentication header expires on: {0}" -f $token.ExpiresOn) -LogId $LogId
+                Write-Log -Message 'Successfully created authentication header' -LogId $LogId
+                Write-Log -Message ("Authentication header expires on: {0}" -f $token.ExpiresOn) -LogId $LogId
                 Write-Host 'Successfully created authentication header' -ForegroundColor Green
-                Write-Host ("Authentication header expires on: {0}" -f $token.ExpiresOn) -ForegroundColor Green
+                Write-Host ("Authentication header expires on (UTC): {0}" -f $token.ExpiresOn.UtcDateTime) -ForegroundColor Green
             }
             catch {
                 Write-Warning -Message ("'{0}'" -f $_.Exception.Message)
-                #Get-ScriptEnd -ErrorMessage 'Failed to create authentication header' -LogId $LogId
+                Get-ScriptEnd -ErrorMessage 'Failed to create authentication header' -LogId $LogId
             }
         }
         else {
-            #Get-ScriptEnd -ErrorMessage 'Failed to connect' -LogId $LogId 
+            Get-ScriptEnd -ErrorMessage 'Failed to connect' -LogId $LogId 
         }
     }
 }
